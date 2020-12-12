e42ec8088e515557d78acc41529595e7a6ac4af0
shokkunrf <19404989+shokkunrf@users.noreply.github.com>
2020-12-12 17:49:20 +0900

    wip

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"tetona/config"
 
 	"github.com/bwmarrin/discordgo"
@@ -37,6 +39,26 @@ func main() {
 	return
 }
 
+type StartResponse struct {
+	StartingInstances []InstanceStatus `json:"StartingInstances"`
+}
+
+type StopResponse struct {
+	StoppingInstances []InstanceStatus `json:"StoppingInstances"`
+}
+
+type InstanceStatus struct {
+	InstanceID   string `json:"InstanceId"`
+	CurrentState struct {
+		Code int    `json:"Code"`
+		Name string `json:"Name"`
+	} `json:"CurrentState"`
+	PreviousState struct {
+		Code int    `json:"Code"`
+		Name string `json:"Name"`
+	} `json:"PreviousState"`
+}
+
 func receive(s *discordgo.Session, event *discordgo.MessageCreate) {
 	messages, err := config.GetConfig()
 	if err != nil {
@@ -45,10 +67,28 @@ func receive(s *discordgo.Session, event *discordgo.MessageCreate) {
 
 	if event.Content == messages.StartTriggerMessage {
 		log.Println("Start Instance")
+		outputJSON, err := exec.Command("aws", "ec2", "start-instances", "--instance-ids", os.Getenv("INSTANCE_ID")).Output()
+		if err != nil {
+			panic(err)
+		}
+
+		startResponse := StartResponse{}
+		if err := json.Unmarshal(outputJSON, &startResponse); err != nil {
+			panic(err)
+		}
 		return
 
 	} else if event.Content == messages.HibernateTriggerMessage {
 		log.Println("Hibernate Instance")
+		outputJSON, err := exec.Command("aws", "ec2", "stop-instances", "--instance-ids", os.Getenv("INSTANCE_ID")).Output()
+		if err != nil {
+			panic(err)
+		}
+
+		stopResponse := StopResponse{}
+		if err := json.Unmarshal(outputJSON, &stopResponse); err != nil {
+			panic(err)
+		}
 		return
 
 	}
