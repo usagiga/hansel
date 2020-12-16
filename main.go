b1979b603639a8bc3bdbd207d4bac105e59e9d94
Sut103 <kou.103.kan@gmail.com>
2020-12-16 13:56:58 +0000

    各パターンでのログだけ整理

--- main.go
+++ main.go
@@ -40,28 +40,71 @@ func receive(s *discordgo.Session, event *discordgo.MessageCreate) {
 	}
 
 	if event.Content == messages.StartTriggerMessage {
-		log.Println("Start Instance")
+		// 起動時
+		log.Println("開始 : インスタンス起動...")
 		outputJSON, err := exec.Command("aws", "ec2", "start-instances", "--instance-ids", os.Getenv("INSTANCE_ID")).Output()
 		if err != nil {
-			log.Fatalln(err)
+			log.Println("起動に失敗した :", err)
+			return
 		}
 
 		startResponse := StartResponse{}
 		if err := json.Unmarshal(outputJSON, &startResponse); err != nil {
-			log.Fatalln(err)
+			log.Println("起動時のレスポンスに異常 :", err)
+			return
+		}
+		currentState := startResponse.StartingInstances[0].CurrentState.Name
+		if currentState == "running" {
+			log.Println("既に起動している")
+			return
 		}
 
+		previousState := startResponse.StartingInstances[0].PreviousState.Name
+		if currentState == "pending" && previousState == "pending" {
+			log.Println("起動処理実行中")
+			return
+		}
+
+		// 開始待ち
+		if _, err := exec.Command("aws", "ec2", "wait", "instance-running", "--instance-ids", os.Getenv("INSTANCE_ID")).Output(); err != nil {
+			log.Println("起動待ちに失敗した :", err)
+			return
+		}
+		log.Println("正常停止 : インスタンス起動")
+
 	} else if event.Content == messages.HibernateTriggerMessage {
-		log.Println("Hibernate Instance")
+		// 停止時
+		log.Println("開始 : インスタンス停止...")
 		outputJSON, err := exec.Command("aws", "ec2", "stop-instances", "--instance-ids", os.Getenv("INSTANCE_ID")).Output()
 		if err != nil {
-			log.Fatalln(err)
+			log.Println("停止に失敗した :", err)
+			return
 		}
 
 		stopResponse := StopResponse{}
 		if err := json.Unmarshal(outputJSON, &stopResponse); err != nil {
-			log.Fatalln(err)
+			log.Println("停止時のレスポンスに異常 :", err)
+			return
 		}
+
+		currentState := stopResponse.StoppingInstances[0].CurrentState.Name
+		if currentState == "stopped" {
+			log.Println("既に停止している")
+			return
+		}
+
+		previousState := stopResponse.StoppingInstances[0].PreviousState.Name
+		if currentState == "stopping" && previousState == "stopping" {
+			log.Println("停止処理実行中")
+			return
+		}
+
+		// 停止待ち
+		if _, err := exec.Command("aws", "ec2", "wait", "instance-stopped", "--instance-ids", os.Getenv("INSTANCE_ID")).Output(); err != nil {
+			log.Println("停止待ちに失敗した :", err)
+			return
+		}
+		log.Println("正常停止 : インスタンス停止")
 	}
 }
 
