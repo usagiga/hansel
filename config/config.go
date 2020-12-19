3f0955867ce19ba0e90d8d1abb3ecc12413b607a
Sut103 <kou.103.kan@gmail.com>
2020-12-19 10:33:27 +0000

    ステータス確認用のメッセージを追加

--- config/config.go
+++ config/config.go
@@ -3,12 +3,14 @@ package config
 type Config struct {
 	StartTriggerMessage     string
 	HibernateTriggerMessage string
+	GetStatusMessage        string
 }
 
 func GetConfig() (Config, error) {
 	config := Config{
 		StartTriggerMessage:     "start",
 		HibernateTriggerMessage: "sleep",
+		GetStatusMessage:        "status",
 	}
 	return config, nil
 }
